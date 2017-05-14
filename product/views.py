a275534f47e9fd36feb0d982287ec4a67ff3b6c1
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-14 16:42:15 -0400

    add product/views.py Delete View

--- product/views.py
+++ product/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # this project
 from product.models import Product
 from shop.models import Shop
@@ -23,6 +23,7 @@ class ProductDetailView(DetailView):
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductModelForm
+    pk_url_kwarg = 'slug'
 
     def dispatch(self, request, *args, **kwargs):
         # if request.user is owner of the shop can publish
@@ -43,6 +44,7 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     fields = ('name', 'categories', 'description', 'photo',)
+    pk_url_kwarg = 'slug'
 
     def dispatch(self, request, *args, **kwargs):
         # if request.user is owner of the shop can update
@@ -53,3 +55,17 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('product:detail', args=(self.object.pk,))
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    pk_url_kwarg = 'slug'
+
+    def dispatch(self, request, *args, **kwargs):
+        # if request.user is owner of the shop can update
+        self.shop = get_object_or_404(Shop, slug=self.kwargs['slug'])
+        if not request.user == self.shop.user:
+            return HttpResponse('404')
+        return super(ProductUpdateView, self).dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('shop:detail', args=self.object.shop.slug)
