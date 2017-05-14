daf9b5d244ab3eba70f796a12519ac99b05aaf10
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-14 15:37:28 -0400

    add users application and his forms.py

--- stores_center/settings.py
+++ stores_center/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'product.apps.ProductConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
