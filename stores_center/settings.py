d2f7e27a3b4b9f4ce5042d6da3556e4a0ca7cd47
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-14 17:47:44 -0400

    fix bug in template dirs

--- stores_center/settings.py
+++ stores_center/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'tamplates')
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
