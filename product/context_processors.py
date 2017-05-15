e7bb90ef2c960b3ff0b41192e346e2547b4a6d93
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-15 16:21:46 -0400

    add context processors ofr menu

---
+++ product/context_processors.py
@@ -0,0 +1,26 @@
+from django.core.urlresolvers import reverse
+from product.models import Category
+
+def menu(request):
+    menu = {'menu': [
+        {'name': 'Home', 'url': reverse('product:list')},
+        {'name': 'New', 'url': '/hola'},
+        {'name': 'Offerts', 'url': '/hola'},
+        {'name': 'Categories', 'url': '/hola'},
+    ]}
+
+    for item in menu['menu']:
+        if item['url'] == request.path:
+            item['active'] = True
+    return menu
+
+
+def categories(request):
+    object = Category.objects.all()
+    categories = {'categories': []}
+    for cat in object:
+        if request.path == cat.slug:
+            categories['categories'].append({'name': cat.title, 'url': '/category/'+cat.slug, 'active': True})
+        else:
+            categories['categories'].append({'name': cat.title, 'url': '/category/'+cat.slug, 'active': False})
+    return categories
