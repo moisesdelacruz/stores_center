dba477d86ef8d34947e84c6584ad3d90ceb2dbfc
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-20 13:51:06 -0400

    add price and discount fields to product/models.py

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
+from djmoney.models.fields import MoneyField
 from django.db import models
 from shop.models import Shop
 import uuid
@@ -10,6 +11,15 @@ def content_file_name(instance, filename):
     return '/'.join(['products', str(instance), filename])
 
 # Create your models here.
+
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
 
 class Category(models.Model):
     title = models.CharField(max_length=45)
@@ -34,6 +44,8 @@ class Product(models.Model):
     name = models.CharField(max_length=60)
     categories = models.ManyToManyField(Category)
     description = models.TextField()
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    discount = IntegerRangeField(blank=True, null=True, min_value=0, max_value=100)
     photo = models.ImageField(upload_to=content_file_name)
     slug = models.SlugField(editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
