4f139927f4037771b7e2b2f2b08948979f020121
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-06-08 00:34:15 -0400

    settings.py > registered shopping_cart.context_processors.cart_products

--- stores_center/settings.py
+++ stores_center/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'product.context_processors.categories',
                 'product.context_processors.menu',
+                'shopping_cart.context_processors.cart_products',
             ],
         },
     },
