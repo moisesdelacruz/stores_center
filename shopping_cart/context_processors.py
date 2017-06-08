1942a80eb676383198f1672e9de29cfe9eebd3e4
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-06-08 15:14:03 -0400

    shopping_cart/context_procesors.py > return list shopping cart - product id.

--- shopping_cart/context_processors.py
+++ shopping_cart/context_processors.py
@@ -2,6 +2,6 @@ from shopping_cart.models import CartProduct
 
 def cart_products(request):
     if request.user.is_authenticated():
-        product = CartProduct.objects.filter(user=request.user)
-        return {'count_product_cart': product.count()}
-    return {'count_product_cart': 0}
+        product = CartProduct.objects.filter(user=request.user).values_list('product__id', flat=True)
+        return {'shopping_cart': product}
+    return {'shopping_cart': []}
