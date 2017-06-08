0573025040af6c239acb5a81c35eac6ddd3abc58
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-06-08 12:21:49 -0400

    shopping_cart/context_processors.py > if not is authenticated so show cart count product 0

--- shopping_cart/context_processors.py
+++ shopping_cart/context_processors.py
@@ -1,5 +1,7 @@
 from shopping_cart.models import CartProduct
 
 def cart_products(request):
-    product = CartProduct.objects.filter(user=request.user)
-    return {'count_product_cart': product.count()}
+    if request.user.is_authenticated():
+        product = CartProduct.objects.filter(user=request.user)
+        return {'count_product_cart': product.count()}
+    return {'count_product_cart': 0}
