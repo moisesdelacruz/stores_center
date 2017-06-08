d56e5b338391ac6e5f4cd36714f5629a92921d2d
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-06-08 09:37:21 -0400

    shopping cart > show product in cart.

--- users/views.py
+++ users/views.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.detail import DetailView
-from users.models import User
 
+from users.models import User
 from shop.models import Shop
+from shopping_cart.models import CartProduct
 
 # Create your views here.
 
@@ -23,4 +24,13 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ProfileDetailView, self).get_context_data(**kwargs)
         context['shops'] = Shop.objects.filter(owner=self.object)
+        self.product_list = CartProduct.objects.filter(user=self.object)
+
+        for index, item in enumerate(self.product_list):
+            # add discount to products
+            if item.product.discount:
+                self.product_list[index].product.new_price = (float(item.product.price)
+                    - ((item.product.discount * float(item.product.price)) / 100))
+
+        context['products'] = self.product_list
         return context
