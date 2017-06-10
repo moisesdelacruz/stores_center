1070f0ce6bfd0b05e037a5295585e4bc859701aa
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-06-10 13:33:49 -0400

    users/views.py > remove cart products

--- users/views.py
+++ users/views.py
@@ -7,7 +7,6 @@ from django.views.generic.detail import DetailView
 
 from users.models import User
 from shop.models import Shop
-from shopping_cart.models import CartProduct
 
 # Create your views here.
 
@@ -24,13 +23,4 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ProfileDetailView, self).get_context_data(**kwargs)
         context['shops'] = Shop.objects.filter(owner=self.object)
-        self.product_list = CartProduct.objects.filter(user=self.object)
-
-        for index, item in enumerate(self.product_list):
-            # add discount to products
-            if item.product.discount:
-                self.product_list[index].product.new_price = (float(item.product.price)
-                    - ((item.product.discount * float(item.product.price)) / 100))
-
-        context['products'] = self.product_list
         return context
